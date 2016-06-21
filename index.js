9d4a0b540cb86ac241353499dd930f9270561a2d
Bjørn-Olav Strand <bo@startsiden.no>
2016-06-21 12:02:21 +0200

    getting version

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 var express = require('express');
 var app = express();
+var https = require('https');
+YAML = require('yamljs');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  // https://github.com/bolav/fuse-sqlite
+  // https://raw.githubusercontent.com/bolav/fuse-sqlite/master/.travis.yml
+  var repo = request.query["repo"];
+  repo = repo.replace('/github.com/', '/raw.githubusercontent.com/');
+  repo = repo + '/master/.travis.yml';
+  console.log(repo);
+  var req = https.get(repo, function(hres) {
+  	var data = '';
+  	hres.on('data', function(d) {
+  		data = data + d;
+  	});
+  	hres.on('end', function() {
+  		var travis = YAML.parse(data);
+  		// console.log(travis.env.global);
+  		var arrayLength = travis.env.global.length;
+  		for (var i = 0; i < arrayLength; i++) {
+  		    var ver = /^FUSE\_VERSION\=(\d+\.\d+\.\d+)/.exec(travis.env.global[i]);
+  		    if (ver) {
+  		    	response.send(ver[1]);
+  		    	return;
+  		    }
+  		    //Do something
+  		} 
+  		response.send('unknown');
+  	});
+  });
+  req.end();
 });
 
 app.listen(app.get('port'), function() {
